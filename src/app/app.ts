8c63684fff68eebc93b1f54cf340f2d9613c101d
Hrsh404 <harshtripathi762@gmail.com>
2025-08-10 18:21:22 +0530

    WishIt Project

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component'; // <-- Import your header
+
+@Component({
+  selector: 'app-root',
+  standalone: true, // standalone root, A root directory!!!! Don't fuck with root!
+  imports: [RouterOutlet, HeaderComponent], 
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss'] 
+})
+export class App {
+  protected readonly title = signal('WishIt');
+}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'; // <-- Needed for *ngFor
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule], // <-- Add this
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  @Input() title = 'Ddsffsfsdfsdfm';
+
+
+
+  //Seach icon starts from here
+
+  onSearch(query: string) {
+    console.log('Searching for:', query);
+  //Seach icon ends from here
+
+  }
+}
