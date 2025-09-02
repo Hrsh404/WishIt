161795c571586ef3aae9232cea7a36cfe7408ee5
vyasverma <94284347+vyasverma@users.noreply.github.com>
2025-09-03 00:05:47 +0530

    Vyas First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { ProductList } from './product-list/product-list';
 
 export const routes: Routes = [
     {path:"", redirectTo:"home",pathMatch:'full'},
     {path:'home', component:Home},
     {path:'login', component:LoginComponent},
     {path:'register', component:RegisterComponent},
-    
+    {path:'products', component:ProductList}
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone:true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.scss'
 })
 export class Home {
+  constructor(public router: Router) {}
 
 }
