3c24eb9a56f12af54115da2fc657c51836fc6f02
ArpitviD <genius.arpitvid@gmail.com>
2025-09-03 21:54:37 +0530

    Cart in Header added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { ProductList } from './product-list/product-list';
+import { Cart } from './header/cart/cart';
 
 export const routes: Routes = [
     {path:"", redirectTo:"home",pathMatch:'full'},
     {path:'home', component:Home},
     {path:'login', component:LoginComponent},
     {path:'register', component:RegisterComponent},
-    {path:'products', component:ProductList}
+    {path:'products', component:ProductList},
+    {path:'cart', component:Cart},
 ];
