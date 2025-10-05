5cb8e75a76a8afb3e5a7acc89baee99a314d339d
ArpitviD <genius.arpitvid@gmail.com>
2025-10-05 13:33:52 +0530

    Product-List

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,19 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { ProductList } from './product-list/product-list';
+
 import { Cart } from './header/cart/cart';
 import { ProfileComponent } from './profile/profile.component';
+import { Products } from './products/products';
+import { ProductList } from './products/product-list/product-list';
 
 export const routes: Routes = [
     {path:"", redirectTo:"home",pathMatch:'full'},
     {path:'home', component:Home},
     {path:'login', component:LoginComponent},
     {path:'register', component:RegisterComponent},
-    {path:'products', component:ProductList},
+    {path:'products', component:Products},
     {path:'cart', component:Cart},
-    {path:'profile', component:ProfileComponent}
+    {path:'profile', component:ProfileComponent},
+    {path:'search', component:ProductList}
 ];
