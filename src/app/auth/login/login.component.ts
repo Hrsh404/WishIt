708b0ea65efcc7bf386e1e3b1073ec8201578438
Roronoa-Zoro-007 <strawhats.rz@gmail.com>
2026-01-18 11:49:37 +0530

    Vendor Registration Bug Fixes

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../auth-service';
 
 @Component({
   selector: 'app-login',
@@ -14,12 +15,19 @@ export class LoginComponent {
     username:'',
     password:''
   }
+
+  constructor(private authService: AuthService){}
+
   loginUser(){
-    if (this.user.username == 'testone' && this.user.password == 'password'){
-      alert('Login Successfull....');
-    }
-    else{
-      alert('Username and password does not match');
-    }
+    this.authService.login(this.user).subscribe({
+      next: (res) => {
+        console.log('Login successful:', res.userData);
+        alert('User logged in successfully!');
+      },
+      error: (err) => {
+        console.error('Error:', err);
+        alert('Something went wrong.');
+      }
+    });
   }
 }
